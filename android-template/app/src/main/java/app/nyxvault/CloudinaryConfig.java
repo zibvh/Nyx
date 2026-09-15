8aac55ab8fbfacd0506815e94b0b280926d1454b
zibvh <bhuszibah@gmail.com>
2026-09-15 15:49:47 +0100

    package lock

--- android-template/app/src/main/java/app/nyxvault/CloudinaryConfig.java
+++
@@ -1,7 +0,0 @@
-package app.nyxvault;
-
-public final class CloudinaryConfig {
-  private CloudinaryConfig() {}
-  public static final String BACKEND_URL = "https://YOUR-NYX-BACKEND.onrender.com";
-  public static final String BACKEND_TOKEN = "PASTE_NYX_APP_TOKEN_HERE";
-}
